c1bd79c12ac921d76cfd4b1b6278b0d9b381ab5b
Arman Sharker <59759586+esdeath28@users.noreply.github.com>
2024-01-06 05:31:58 +0600

    added api to get temperatures at 2pm

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('addDistrict', views.addDistrict),
     path('getDistricts', views.getDistricts),
+    path('getTemperatureAt2PM', views.getTemperatureAt2PM),
     path('getCoolestDistrict', views.getCoolestDistrict),
+
 ]
--- api/views.py
+++ api/views.py
@@ -2,12 +2,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from appbase.models import District
 from .serializers import DistrictSerializer
+import requests
+
+def get_districts_data():
+    districts = District.objects.all()
+    serializer = DistrictSerializer(districts, many=True)
+    return serializer.data
 
 @api_view(['GET'])
 def getDistricts(request):
-    districts = District.objects.all()
-    serializer = DistrictSerializer(districts, many=True)
-    return Response(serializer.data)
+    districts_data = get_districts_data()
+    return Response(districts_data)
 
 @api_view(['POST'])
 def addDistrict(request):
@@ -27,3 +32,27 @@ def getCoolestDistrict(request):
         "long": "90.4111451"
     }
     return Response(coolestDistrict)
+
+@api_view(['GET'])
+def getTemperatureAt2PM(request):
+    districts_data = get_districts_data()
+
+    latitude = [float(district['lat']) for district in districts_data]
+    longitude = [float(district['long']) for district in districts_data]
+    hour = 14
+
+    endpoint = "https://api.open-meteo.com/v1/forecast"
+    url_parameters = "&".join([f"latitude={lat}&longitude={lon}&hourly=temperature_2m" for lat, lon in zip(latitude, longitude)])
+    api_request = f"{endpoint}?{url_parameters}"
+
+    meteo_data = requests.get(api_request).json()
+
+    temperatures = [
+        [data['hourly']['temperature_2m'][hour + 24 * day] for day in range(7)]
+        for data in meteo_data
+    ]
+    result = [
+        {"district": district['name'], "temperatures_at_2pm": temp_data}
+        for district, temp_data in zip(districts_data, temperatures)
+    ]
+    return Response({"temperatures": result})
