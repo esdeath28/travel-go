c924d126c8a2249024cb169c114391f3c69553ee
Arman Sharker <59759586+esdeath28@users.noreply.github.com>
2024-01-06 06:15:44 +0600

    added api to get 10 coolest districts based on average temperatures at 2pm

--- api/views.py
+++ api/views.py
@@ -23,15 +23,33 @@ def addDistrict(request):
 
 @api_view(['GET'])
 def getCoolestDistrict(request):
-    coolestDistrict = {
-        "id": "1",
-        "division_id": "3",
-        "name": "Dhaka",
-        "bn_name": "ঢাকা",
-        "lat": "23.7115253",
-        "long": "90.4111451"
+    districts_data = get_districts_data()
+
+    latitude = [float(district['lat']) for district in districts_data]
+    longitude = [float(district['long']) for district in districts_data]
+    hour = 14
+
+    endpoint = "https://api.open-meteo.com/v1/forecast"
+    url_parameters = "&".join([f"latitude={lat}&longitude={lon}&hourly=temperature_2m" for lat, lon in zip(latitude, longitude)])
+    api_request = f"{endpoint}?{url_parameters}"
+
+    meteo_data = requests.get(api_request).json()
+
+    temperatures = [
+        [data['hourly']['temperature_2m'][hour + 24 * day] for day in range(7)]
+        for data in meteo_data
+    ]
+    average_temperatures = [sum(temp_list) / len(temp_list) for temp_list in temperatures]
+    result = [
+    {
+        "district": district['name'],
+        "temperatures_at_2pm": temp_data,
+        "average_temperature_at_2pm": avg_temp
     }
-    return Response(coolestDistrict)
+    for district, temp_data, avg_temp in zip(districts_data, temperatures, average_temperatures)
+    ]
+    coolest_district = sorted(result, key=lambda x: x['average_temperature_at_2pm'])
+    return Response({"Coolest Districts": coolest_district[:10]})
 
 @api_view(['GET'])
 def getTemperatureAt2PM(request):
@@ -46,7 +64,8 @@ def getTemperatureAt2PM(request):
     api_request = f"{endpoint}?{url_parameters}"
 
     meteo_data = requests.get(api_request).json()
-
+    # temperatures = [data['hourly']['temperature_2m'][hour] for data in meteo_data]
+    # result = [{"district": district['name'], "temperatures_at_2pm": temperatures} for district in districts_data]
     temperatures = [
         [data['hourly']['temperature_2m'][hour + 24 * day] for day in range(7)]
         for data in meteo_data
